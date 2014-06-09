4293b101755bf07ad5478cbc728154cd8f259fba
acavanaghww <acavanagh@weddingwire.com>
2014-06-09 11:10:51 -0700

    Initial Commit

---
+++ SwiftSort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSort
+//
+//  Created by Andrew Cavanagh on 6/9/14.
+//  Copyright (c) 2014 WeddingWire. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
